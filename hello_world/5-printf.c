adc22d0b184672f688d2de3445eb40c4adbc7063
Maurlcio <thecoolermess@gmail.com>
2022-09-27 08:05:35 -0700

    betty sure is picky

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
-* main - prints specific text
-* Return: Zilch. 
+/**
+*main - prints specific text.
+*
+*Return: Zilch.
 */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return(0);
-} /n
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+/ n
