d3dfa6ed39108400b9a65c99727f5406a00a75c0
Maurlcio <thecoolermess@gmail.com>
2022-09-28 09:23:43 -0700

    prints alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - shows alphabet in lc, then newline.
+*Return: yes.
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar('\n');
+return (0);
+}
