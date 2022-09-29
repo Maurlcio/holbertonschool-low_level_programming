1c6f3ad8beaf6fe98ce2b9a83372d359a9150e68
Maurlcio <thecoolermess@gmail.com>
2022-09-29 07:10:34 -0700

    :D

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
