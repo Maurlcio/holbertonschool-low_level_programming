b54b08aea8811ea3cb1a34f51d3c19dc16697c0a
Maurlcio <thecoolermess@gmail.com>
2022-09-29 10:07:38 -0700

    computes the absolute value of any integer, which is basically just saying 'is this positive? no? well fuck you now you are'

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs - computes the absolute value of an integer
+*@n: the value to compute
+*Return: positive or negative times negative. a classic
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else if (n < 0)
+return (n * -1);
+}
