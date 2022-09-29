ad9612a0e93c1713409d0337df1b4c287fc62b0d
Maurlcio <thecoolermess@gmail.com>
2022-09-29 07:58:09 -0700

    loops within loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - ten times the alphabet. Ten halves the price.
+*/
+
+void print_alphabet_x10(void)
+{
+int n;
+char ch;
+
+for (n = 0; n <= 9; n++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
