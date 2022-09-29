48b50584493676ca02f7a9a888ba78932a1865e0
Maurlcio <thecoolermess@gmail.com>
2022-09-29 08:51:51 -0700

    phew

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,18 @@
 
 /**
 *_islower - checks if a letter is lowercase
+*@c: yuh
+*Return: 0 if false, 1 if not false
 */
 
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return (1)
+return (1);
 }
-return (0)
+else
+{
+return (0);
+}
 }
