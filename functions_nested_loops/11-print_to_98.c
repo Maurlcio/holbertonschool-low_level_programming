7d560724f27cb9ae7c25e077c23e156e8c9137e7
Maurlcio <thecoolermess@gmail.com>
2022-09-30 05:43:50 -0700

    very fun!

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+*print_to_98 - print_to_98
+*@n: 98 (or not 98)
+*/
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
+}
+}
+else if
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+printf("\n");
+}
