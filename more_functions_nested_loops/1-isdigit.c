2155b3ca95c50508c584035065006087ef60b47d
Maurlcio <thecoolermess@gmail.com>
2022-09-30 09:23:07 -0700

    best rated 1997

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isdigit - are you a single number? no? well you will be soon!
+*@c: c
+*Return: binary moment
+*/
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
