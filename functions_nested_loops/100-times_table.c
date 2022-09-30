666b6a5d19430327e5bb7a0b8781b5e1162a6ffa
Maurlcio <thecoolermess@gmail.com>
2022-09-30 06:25:06 -0700

    you guys BAD for this one

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+*print_times_table - like the other one but BIG
+*@n: da int
+*/
+
+void print_times_table(int n)
+{
+int num1;
+int num2;
+
+if (n <= 15 && n >= 0)
+{
+for (num1 = 0; num1 <= n; num1++)
+{
+for (num2 = 0; num2 <= n; num2++)
+{
+int prod = num1 * num2;
+
+if (num2 == 0)
+{
+_putchar('0'); }
+else if (prod <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + prod); }
+else if (prod > 99)
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + (prod / 100));
+_putchar('0' + ((prod / 10) % 10));
+_putchar('0' + (prod % 10)); }
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + (prod / 10));
+_putchar('0' + (prod % 10)); }
+}
+_putchar('\n');
+}
+}
+}
