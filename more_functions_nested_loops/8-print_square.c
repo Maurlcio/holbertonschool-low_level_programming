b05215d631ca1c6c2876cbc7567498d9379ec333
Maurlcio <thecoolermess@gmail.com>
2022-09-30 10:40:12 -0700

    its hip

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_square - its hip
+*@size: MASSIVE
+*/
+
+void print_square(int size)
+{
+int num1;
+int num2;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (num1 = 0; num1 < size; num1++)
+{
+for (num2 = 0; num2 < size; num2++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
