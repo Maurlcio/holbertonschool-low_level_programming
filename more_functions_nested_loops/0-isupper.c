2b8339fb872442a40b602969fb1c7e8837ec4f4b
Maurlcio <thecoolermess@gmail.com>
2022-09-30 09:14:10 -0700

    fun throwback

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isupper - checks for uppercase letter
+*@c: integer
+*Return: 1 if true, 0 if un-so
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
