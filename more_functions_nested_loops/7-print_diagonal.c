c50b66131af8f4af97c3e18dfd360751307c40e6
Maurlcio <thecoolermess@gmail.com>
2022-09-30 10:29:52 -0700

    i cant do the diagonal line on this lol

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_diagonal - ok i cant do this one without fucking up the comments
+*@n: n
+*/
+
+void print_diagonal(int n)
+{
+int slash;
+int space;
+
+if (n > 0)
+{
+for (slash = 0; slash < space; slash++)
+{
+for (space = 0; space < slash; space++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
