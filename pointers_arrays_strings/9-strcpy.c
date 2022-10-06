5002ef86b123ac9eda149b1037ffec2314a46ab9
Maurlcio <thecoolermess@gmail.com>
2022-10-06 09:50:58 -0700

    idk this one was kind of all over the place i think

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+*_strcpy - copies a string
+*@dest: destination
+*@src: pretty self-explanatory
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int times;
+
+for (times = 0; times <= _strlen(src); times++)
+{
+dest[times] = src[times];
+}
+return (dest);
+}
