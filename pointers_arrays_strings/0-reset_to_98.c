9c36788d910f3de03c71b213c6873a769f1306ed
Maurlcio <thecoolermess@gmail.com>
2022-10-06 06:17:16 -0700

    easy paeasy

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*reset_to_98 - takes a pointer of type int and changes its value to 98.
+*@n: the type int variable named "n"
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
