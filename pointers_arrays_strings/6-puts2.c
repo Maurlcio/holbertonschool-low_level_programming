e4c10667a08693731a6da0c8158949a6ea8af2d1
Maurlcio <thecoolermess@gmail.com>
2022-10-06 09:18:58 -0700

    all my homies hate every other character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main-h"
+
+/**
+*puts2 - prints every other character of a string
+*@str: pointer
+*/
+
+void puts2(char *str)
+{
+int evryotr;
+
+for (evryotr = 0; evryotr < strlen(str); evryotr++)
+{
+if (evryotr % 2 == 0)
+{
+putchar(str[evryotr]);
+}
+}
+putchar('\n');
+}
