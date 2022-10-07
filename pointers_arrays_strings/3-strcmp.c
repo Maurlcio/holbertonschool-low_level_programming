7e46d6f126acac5a2a761da240a2346e8d0e66e5
Maurlcio <thecoolermess@gmail.com>
2022-10-07 06:56:52 -0700

    i like this one

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 if (strlen(s1) == strlen(s2))
 return (0);
 else if (strlen(s1) < strlen(s2))
-return (strlen(s1) - strlen(s2));
-else
 return (strlen(s1) + strlen(s2));
+else
+return (strlen(s1) - strlen(s2));
 }
