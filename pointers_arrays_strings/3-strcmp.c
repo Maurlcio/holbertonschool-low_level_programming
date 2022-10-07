68cbb9dd6f6dfdf6ebec78b4b8329c47cc9a117f
Maurlcio <thecoolermess@gmail.com>
2022-10-07 06:46:50 -0700

    i like this one

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strcmp - compares two strings
+*@s1: string 1
+*@s2: string 2
+*Return: i am very int-elligent
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+if (strlen(s1) == strlen(s2))
+{
+return (0);
+}
+else if (strlen(s1) < strlen(s2))
+{
+return (-1);
+}
+else if (strlen(s1) > strlen(s2))
+{
+return (1);
+}
+}
