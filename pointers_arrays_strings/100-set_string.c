58e27a83df1c2ac510a757943676c49ce36af7ea
Maurlcio <thecoolermess@gmail.com>
2022-10-11 05:20:28 -0700

    appreciate the freebie!

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*set_sring - sets value of a pointer to a char, kinda like frankenstein i think
+*@s: s
+*@to: to
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
