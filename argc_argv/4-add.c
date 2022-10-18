219cd4abcac076d4cc944dd94af058a1fb214379
Maurlcio <thecoolermess@gmail.com>
2022-10-18 08:22:36 -0700

    adds numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,28 +2,30 @@
 
 /**
 *main - adds numbers
-*argc: argument count
-*argv: argument vector
+*@argc: argument count
+*@argv: argument vector
 *Return: 1 if ur stupid, 0 if true
 */
 
 int main(int argc, char *argv[])
 {
-int a, b, tosum = 0;
+int a, b, c = 0;
 
 for (a = 1; a < argc; a++)
 {
 for (b = 0; argv[a][b]; b++)
 {
-if (isdigit(argv[a][b]))
+if (isdigit(argv[a][b]) == 0)
 {
 printf("Error\n");
 return (1);
 }
 }
-tosum += atoi(argv[a]);
 }
-printf("%d\n", tosum);
+for (a = 1; a < argc; a++)
+{
+c += atoi(argv[a]);
+}
+printf("%d\n", c);
 return (0);
 }
-  
