26e0f0c01a8ad8da00ab215441638cecc64ff1c1
Maurlcio <thecoolermess@gmail.com>
2022-10-18 06:37:00 -0700

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main - prints argc essentially
+*@argc: argument count
+*@argv: argument vector
+*Return: zerooooo
+*/
+
+int main(int argc, char *argv[])
+{
+argv = argv;
+printf("%d\n", argc - 1);
+return (0);
+}
