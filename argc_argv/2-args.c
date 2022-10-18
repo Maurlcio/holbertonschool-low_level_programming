8a03fbe25c2290afc1e1ea3c553ca1cf0d848359
Maurlcio <thecoolermess@gmail.com>
2022-10-18 06:53:58 -0700

    commitment to the bit

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+*main - prints all arguements
+*@argc: argument count
+*@argv: arguement vector
+*Return: zero
+*/
+
 int main(int argc, char *argv[])
 {
 int count;
 
-for (count = 0; count <= argc; count++)
+for (count = 0; count < argc; count++)
 {
 printf("%s\n", argv[count]);
 }
