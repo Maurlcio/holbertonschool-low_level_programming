aef6042143b52957264c0d367867cdfff2b0410f
Maurlcio <thecoolermess@gmail.com>
2022-10-18 07:17:11 -0700

    multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 int num1;
 int num2;
 int result;
-  
+
 if (argc != 3)
 {
 printf("Error\n");
@@ -22,7 +22,7 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = (num1 * num2);
+result = num1 * num2;
 
 printf("%d\n", result);
 return (0);
