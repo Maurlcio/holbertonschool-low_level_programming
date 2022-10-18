1fb6b56ecf0d28bf857b33a58968dc6b63bc5a05
Maurlcio <thecoolermess@gmail.com>
2022-10-18 08:10:51 -0700

    adds numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*main - adds numbers
+*argc: argument count
+*argv: argument vector
+*Return: 1 if ur stupid, 0 if true
+*/
+
+int main(int argc, char *argv[])
+{
+int a, b, tosum = 0;
+
+for (a = 1; a < argc; a++)
+{
+for (b = 0; argv[a][b]; b++)
+{
+if (isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+tosum += atoi(argv[a]);
+}
+printf("%d\n", tosum);
+return (0);
+}
+  
