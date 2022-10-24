61ff38512b696475ef51e7b584f0fa5ee9d5c72e
Maurlcio <thecoolermess@gmail.com>
2022-10-24 08:35:40 -0700

    yeah

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -138,3 +138,7 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grind(int width, int height);
 
 void free_grid(int **grid, int height);
+
+char **strtow(char *str);
+
+char *argstostr(int ac, char **av);
