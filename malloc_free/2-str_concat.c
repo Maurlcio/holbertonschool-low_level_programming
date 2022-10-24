2b4f6284ddcb6cc2a3b9bef6337d2b757dd8fe0b
Maurlcio <thecoolermess@gmail.com>
2022-10-24 07:12:23 -0700

    concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,18 +9,18 @@
 
 char *str_concat(char *s1, char *s2)
 {
-unsigned int a, b, c, d;
+int a, b, c, d;
 char *str;
 
 if (s1 == NULL)
-a = "";
+a = 0;
 else
 {
 for (a = 0; s1[a]; a++)
 {
 }
 if (s2 == NULL)
-b = "";
+b = 0;
 else
 {
 for (b = 0; s2[b]; b++)
