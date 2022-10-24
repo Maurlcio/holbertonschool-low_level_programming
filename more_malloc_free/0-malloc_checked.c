752f1cadf7e00cf7463077e0d9b58d5e9acf3b5c
Maurlcio <thecoolermess@gmail.com>
2022-10-24 09:34:43 -0700

    easy!

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*malloc_checked - does what malloc literally does
+*@b: bytes
+*/
+
+void *malloc_checked(unsigned int b)
+{
+char *vessel;
+
+p = malloc(vessel);
+if (vessel == NULL)
+{
+exit 98;
+return (vessel);
+}
