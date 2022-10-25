7a11d8cc0be51d233524501e07067df4a7bde14c
Maurlcio <thecoolermess@gmail.com>
2022-10-25 06:34:16 -0700

    yeah

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -146,3 +146,9 @@ char *argstostr(int ac, char **av);
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmembm unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
