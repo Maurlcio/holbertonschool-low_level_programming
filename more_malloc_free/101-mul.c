9eba222cdbc0931324cb7f7a6491603b3aa5d870
Maurlcio <thecoolermess@gmail.com>
2022-10-26 05:51:47 -0700

    This one is pretty interesting, especially with the command restrictions put in place, since you essentially need to make at least one function that's gonna serve the role of stuff like strlen and such, and I still only used them like, once lol. Massive headache this one

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -66,7 +66,10 @@ len2 = _strlen(s2);
 len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
 if (!result)
+{
 return (1);
+free(result);
+}
 for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
