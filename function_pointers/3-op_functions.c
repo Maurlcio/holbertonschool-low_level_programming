07f84610cb457d1b4e17e416a2dbca3c1614dbd3
Maurlcio <thecoolermess@gmail.com>
2022-10-27 09:51:32 -0700

    op, op op op

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - baby math for smart babies
+ * @a: a
+ * @b: b
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - more math
+ * @a: a
+ * @b: b
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - second grade math now lets gooo
+ * a@a: a
+ * @b: b
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - seriously what do i even write anymore
+ * @a: you think this is funny?? i have a degree you know
+ * @b: i mean, i actually dont, but imagine if i did
+ * Return: wouldn't that be nice
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - ok this one would actually slightly trump a child maybe
+ * @a: kind of, anyway
+ * @b:i mean... maybe??
+ * Return: module
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
