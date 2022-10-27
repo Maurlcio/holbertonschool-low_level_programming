7539a5fb5f4fe1343650d3d3304e36042670a489
Maurlcio <thecoolermess@gmail.com>
2022-10-27 05:48:31 -0700

    I'm officially making the switch to Vi, so if my code looks wildly different in terms of visual hierarchy, you'll know why

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog!
+ * @name: Lucky
+ * @age: four
+ * @owner: Mauricio de Betolaza
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
