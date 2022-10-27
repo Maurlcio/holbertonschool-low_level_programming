713a29f3d7f7e48ad93ee5571f6b81b345518f36
Maurlcio <thecoolermess@gmail.com>
2022-10-27 07:54:41 -0700

    this warmed my heart a lot

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,12 +3,16 @@
 #include <stdio.h>
 
 /**
- *
+ * free_dog - free!!!!!!!!!!
+ * @d: pointer stand-in for dog_t
  */
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
