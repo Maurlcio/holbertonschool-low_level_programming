a9a17d8049d076a2e65de99bbca3387df8b55833
Maurlcio <thecoolermess@gmail.com>
2022-10-27 09:00:08 -0700

    executes multiple functions within an array of 'em

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function on each element of the array
+ * @array: pointer to the array
+ * @size: of the array
+ * @action: pointer to the function used
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a;
+
+	for (a = 0; a < size; a++)
+	{
+		action(array[a]);
+	}
+}
