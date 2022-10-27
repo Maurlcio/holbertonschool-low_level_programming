2b899db440c654b7e4e9ecc297582b0aa7d90987
Maurlcio <thecoolermess@gmail.com>
2022-10-27 06:08:22 -0700

    dogs make literally any exercise better

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * init_dog - initializes a variable of type struct
+ * @d: pointer to the struct
+ * @name: love dogs
+ * @age: looove dogs
+ * @owner: love 'em
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
