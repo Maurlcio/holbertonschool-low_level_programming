f20bc08add0aba3bdeb4ec5eede1a13849baa223
Maurlcio <thecoolermess@gmail.com>
2022-10-27 10:17:28 -0700

    yeah

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VAR_FUN
 #define VAR_FUN
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
