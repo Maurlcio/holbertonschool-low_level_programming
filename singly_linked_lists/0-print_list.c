ee01a05b8df25bd33381bc3c4b655d1f9b4c07ea
Maurlcio <thecoolermess@gmail.com>
2022-11-14 09:44:57 -0800

    Prints all the elements of a rather specific list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t retrn;
+	size_t retrn = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		retrn++;
