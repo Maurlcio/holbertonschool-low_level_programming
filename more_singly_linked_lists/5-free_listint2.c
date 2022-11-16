80a5ca79568c710402e2c6c4bfc54f406b06a105
Maurlcio <thecoolermess@gmail.com>
2022-11-16 08:17:55 -0800

    freedom motif

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list but also sets head to nil
+ * @head: So tall we'll see past the dark! Stare right into...
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux = *head;
+
+	while (aux != NULL)
+	{
+		listint_t *aux2 = aux;
+
+		aux = aux->next;
+
+		free(aux2);
+	}
+	*head = NULL;
+}
